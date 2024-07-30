3cea5505a695773addad06e440fbc58434ed9da2
Mathius <info@yogimathius.dev>
2024-07-30 00:47:58 -0700

    feat: hook up ksqldb to kafka container (wip)

--- create-topics.sh
+++ create-topics.sh
@@ -16,5 +16,5 @@ KAFKA_TOPICS=(
 
 # Loop over each topic and create it
 for TOPIC in "${KAFKA_TOPICS[@]}"; do
-  /opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic "$TOPIC" --replication-factor 1 --partitions 3 --bootstrap-server kafka:9092
+  /usr/bin/kafka-topics --create --if-not-exists --topic "$TOPIC" --replication-factor 1 --partitions 3 --bootstrap-server kafka:29092
 done
