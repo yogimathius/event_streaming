b05f420aaa7ef1c919acbce7358efd613816d0ce
Mathius <info@yogimathius.dev>
2024-08-05 22:37:19 -0700

    feat: add event logging for each step in consumer

--- rust-consumer/src/consumer.rs
+++ rust-consumer/src/consumer.rs
@@ -1,8 +1,12 @@
-use crate::worker::{Event, Priority, Transmitter};
+use crate::{
+    producer::KafkaProducer,
+    worker::{Event, Priority, Transmitter},
+};
 use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 
 pub struct KafkaConsumer {
     consumer: Consumer,
+    producer: KafkaProducer,
 }
 
 impl KafkaConsumer {
@@ -16,8 +20,8 @@ impl KafkaConsumer {
             .with_offset_storage(Some(GroupOffsetStorage::Kafka))
             .create()
             .unwrap();
-
-        KafkaConsumer { consumer }
+        let producer = KafkaProducer::new();
+        KafkaConsumer { consumer, producer }
     }
 
     pub fn poll(&mut self, tx: &Transmitter) {
@@ -26,7 +30,10 @@ impl KafkaConsumer {
                 for m in ms.messages() {
                     let payload = std::str::from_utf8(m.value).unwrap();
                     match serde_json::from_str::<Event>(payload) {
-                        Ok(event) => {
+                        Ok(mut event) => {
+                            event.status = "consumed".to_owned();
+                            event.event_time = chrono::Utc::now().to_rfc3339();
+                            self.producer.send(event.clone());
                             self.process_event(event, tx);
                         }
                         Err(e) => {
@@ -46,26 +53,35 @@ impl KafkaConsumer {
         }
     }
 
-    fn process_event(&self, event: Event, tx: &Transmitter) {
+    fn process_event(&mut self, event: Event, tx: &Transmitter) {
         match event.priority {
-            Priority::High => self.delegate_to_high_worker(event, tx),
-            Priority::Medium => self.delegate_to_med_worker(event, tx),
-            Priority::Low => self.delegate_to_low_worker(event, tx),
+            Priority::High => self.delegate_to_high_worker(event.clone(), tx),
+            Priority::Medium => self.delegate_to_med_worker(event.clone(), tx),
+            Priority::Low => self.delegate_to_low_worker(event.clone(), tx),
         }
     }
 
-    fn delegate_to_high_worker(&self, event: Event, tx: &Transmitter) {
+    fn delegate_to_high_worker(&mut self, mut event: Event, tx: &Transmitter) {
         println!("Delegated high priority event: {:?}", event);
-        tx.send(event);
+        tx.send(event.clone());
+        event.status = "delegated high priority".to_owned();
+        event.event_time = chrono::Utc::now().to_rfc3339();
+        self.producer.send(event);
     }
 
-    fn delegate_to_med_worker(&self, event: Event, tx: &Transmitter) {
+    fn delegate_to_med_worker(&mut self, mut event: Event, tx: &Transmitter) {
         println!("Delegated medium priority event: {:?}", event);
-        tx.send(event);
+        tx.send(event.clone());
+        event.status = "delegated medium priority".to_owned();
+        event.event_time = chrono::Utc::now().to_rfc3339();
+        self.producer.send(event);
     }
 
-    fn delegate_to_low_worker(&self, event: Event, tx: &Transmitter) {
+    fn delegate_to_low_worker(&mut self, mut event: Event, tx: &Transmitter) {
         println!("Delegated low priority event: {:?}", event);
-        tx.send(event);
+        tx.send(event.clone());
+        event.status = "delegated low priority".to_owned();
+        event.event_time = chrono::Utc::now().to_rfc3339();
+        self.producer.send(event);
     }
 }
