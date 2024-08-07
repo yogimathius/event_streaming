a56a44481362976c0a8616af076fb66d29883827
Mathius <info@yogimathius.dev>
2024-08-06 17:08:57 -0700

    chore: add error handling in producer.rs

--- rust-consumer/src/producer.rs
+++ rust-consumer/src/producer.rs
@@ -12,7 +12,7 @@ impl KafkaProducer {
             .with_ack_timeout(Duration::from_secs(1))
             .with_required_acks(RequiredAcks::One)
             .create()
-            .unwrap();
+            .expect("Failed to create Kafka producer");
 
         KafkaProducer { producer }
     }
@@ -20,8 +20,11 @@ impl KafkaProducer {
     pub fn send(&mut self, payload: Event) {
         let topic = payload.clone().event_type;
         let payload = serde_json::to_string(&payload).unwrap();
-        let record = Record::from_value(&topic, payload.as_bytes());
+        let record = Record::from_value(&topic, payload.as_bytes()).with_partition(1);
+        println!("Sending payload: {:?}", record);
 
-        self.producer.send(&record).unwrap();
+        if let Err(e) = self.producer.send(&record) {
+            println!("Failed to send message to Kafka: {:?}", e);
+        }
     }
 }
