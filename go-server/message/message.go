9773cb5c90da1d7fa1244edb26548382c9408039
Mathius <info@yogimathius.dev>
2024-08-11 00:00:25 -0700

    chore: add EventId to message

--- go-server/message/message.go
+++ go-server/message/message.go
@@ -4,6 +4,7 @@ import "time"
 
 type Message struct {
 	Id          string    `json:"id"`
+	EventId 	  int    `json:"event_id"`
 	EventType   string    `json:"event_type"`
 	EventTime   time.Time `json:"event_time"`
 	Priority    string    `json:"priority"`
