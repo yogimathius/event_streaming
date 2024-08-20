e3bddcfe05c472a8fc24a77face560b73a17b377
Mathius <info@yogimathius.dev>
2024-08-20 14:55:57 -0700

    refactor: update logic in pool (wip)

--- go-worker/pool.go
+++ go-worker/pool.go
@@ -29,9 +29,26 @@ type WorkerPool struct {
 	priority       string
 	db             *sql.DB
 	producer       Producer
+	routineType    struct {
+		Working time.Duration
+		Idling  time.Duration
+	}
 }
 
-func NewWorkerPool(numWorkers int, rdb *redis.Client, ctx context.Context, priority string, db *sql.DB, producer Producer) *WorkerPool {
+func NewWorkerPool(numWorkers int, rdb *redis.Client, ctx context.Context, priority string, db *sql.DB, producer Producer, routineType string) *WorkerPool {
+	routinePeriods := map[string]struct{
+		Working time.Duration
+		Idling  time.Duration
+	}{
+		"Standard":    {20 * time.Second, 5 * time.Second},
+		"Intermittent": {5 * time.Second, 5 * time.Second},
+		"Concentrated": {60 * time.Second, 60 * time.Second},
+	}
+
+	if _, exists := routinePeriods[routineType]; !exists {
+		panic("Invalid routine type")
+	}
+
 	pool := &WorkerPool{
 		workers:    make([]*Worker, numWorkers),
 		taskQueue:  make(chan string, 100),
@@ -41,9 +58,9 @@ func NewWorkerPool(numWorkers int, rdb *redis.Client, ctx context.Context, prior
 		priority:   priority,
 		db:         db,
 		producer:   producer,
+		routineType:  routinePeriods[routineType],
 	}
-
-	for i := 0; i < numWorkers; i++ {
+	for i := 0; i < 2; i++ {
 		worker := &Worker{
 			ID:          i + 1,
 			RoutineType: "Standard",
@@ -54,23 +71,43 @@ func NewWorkerPool(numWorkers int, rdb *redis.Client, ctx context.Context, prior
 		go pool.workerRoutine(worker)
 	}
 
+	// for i := 2; i < numWorkers; i++ {
+	// 	worker := &Worker{
+	// 		ID:          i + 1,
+	// 		RoutineType: "Standard",
+	// 		Active:      true,
+	// 		ctx:         ctx,
+	// 	}
+	// 	pool.workers[i] = worker
+	// 	time.Sleep(10 * time.Second)
+	// 	go pool.workerRoutine(worker)
+	// }
+
 	return pool
 }
 
 func (wp *WorkerPool) workerRoutine(worker *Worker) {
-	for worker.Active {
+	time.Sleep(wp.routineType.Working)
+	
+	workingTimer := time.NewTimer(wp.routineType.Idling)
+	defer workingTimer.Stop()
+
+	for worker.Active {        
+
 		select {
 		case task := <-wp.taskQueue:
 			wp.processMessage(task)
 		case <-worker.ctx.Done():
 			fmt.Printf("Worker %d stopped due to context cancellation\n", worker.ID)
 			return
+		case <-workingTimer.C:
+			time.Sleep(wp.routineType.Working)
+			workingTimer.Reset(wp.routineType.Idling)
 		}
 	}
 }
 
 func (wp *WorkerPool) processMessage(msgString string) {
-	fmt.Printf("Processing message: %s\n", msgString)
 	var msg message.Message
 
 	msgBytes := []byte(msgString)
@@ -79,17 +116,28 @@ func (wp *WorkerPool) processMessage(msgString string) {
 		log.Printf("Error unmarshalling message: %v\n", err)
 		return
 	}
+	var timeToComplete int
 
-	fmt.Printf("Processed message: %v\n", msg)
-
-	if msg.EventTime.Before(time.Now().Add(-6 * time.Second)) {
-		fmt.Printf("msg %v is older than 6 seconds\n", msg)
+	switch msg.Priority {
+	case "High":
+			timeToComplete = -5
+	case "Medium":
+			timeToComplete = -10
+	case "Low":
+			timeToComplete = -15
+	default:
+			timeToComplete = 0 // Default value if priority doesn't match any case
+	}
+	
+	if msg.EventTime.Before(time.Now().Add(time.Duration(timeToComplete) * time.Second)) {
+		fmt.Printf("!!!!MSG %v IS OLDER THAN %d SECONDS!!!!\n", msg.Status, timeToComplete)
 		msg.Status = "message unresolved"
 		db.AddEventMessage(wp.db, 1, msg)
 		return
 	}
 
-	msg.Status = fmt.Sprintf("message completed by %s priority worker", msg.Priority)
+	msg.Status = fmt.Sprintf("message completed by %s priority worker", wp.priority)
+	fmt.Printf("msg %v is completed by %s worker\n", msg.EventType, wp.priority)
 	if err := wp.producer.SendMessage(msg); err != nil {
 		fmt.Printf("Failed to send message to producer: %v\n", err)
 	}
@@ -100,10 +148,9 @@ func (wp *WorkerPool) processMessage(msgString string) {
 func (wp *WorkerPool) Run() {
 	fmt.Println("Worker pool started")
 	for {
-		timeout := time.Duration(5) * time.Second
-		message, err := wp.rdb.BRPop(wp.ctx, timeout, wp.priority).Result()
+		time := wp.routineType.Working
+		message, err := wp.rdb.BRPop(wp.ctx, time, wp.priority).Result()
 		if err != nil {
-			fmt.Printf("Waiting for messages in Redis queue\n")
 			continue
 		}
 
