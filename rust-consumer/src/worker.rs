28189ee4599d50644de0e9b203b1e1ecbd76da75
Mathius <info@yogimathius.dev>
2024-09-03 22:53:20 -0700

    refactor: cleanup worker.rs

--- rust-consumer/src/worker.rs
+++ rust-consumer/src/worker.rs
@@ -1,37 +1,11 @@
-use serde::{Deserialize, Serialize};
+use chrono::Utc;
+use crossbeam_channel::unbounded;
 use std::{
-    collections::VecDeque,
-    sync::{Arc, Condvar, Mutex},
+    sync::{Arc, Mutex},
     thread,
 };
-use uuid::Uuid;
 
-use crate::producer::KafkaProducer;
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct Event {
-    pub event_type: String,
-    pub priority: Priority,
-    description: String,
-    pub status: String,
-    pub event_time: String,
-    pub id: Uuid,
-}
-
-pub struct Transmitter {
-    store: Arc<Mutex<VecDeque<Event>>>,
-    emitter: Arc<Condvar>,
-}
-
-pub struct Receiver {
-    store: Arc<Mutex<VecDeque<Event>>>,
-    emitter: Arc<Condvar>,
-}
-
-pub struct Channel {
-    pub tx: Transmitter,
-    pub rx: Receiver,
-    producer: KafkaProducer,
-}
+use crate::{producer::KafkaProducer, receiver::Receiver, transmitter::Transmitter};
 
 #[derive(Debug, Copy, Clone)]
 pub enum RoutineType {
@@ -40,117 +14,53 @@ pub enum RoutineType {
     Concentrated,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub enum Priority {
-    High,
-    Medium,
-    Low,
-}
-
-impl Priority {
-    pub fn as_str(&self) -> &str {
-        match self {
-            Priority::High => "High",
-            Priority::Medium => "Medium",
-            Priority::Low => "Low",
-        }
-    }
-}
-
-impl Event {
-    pub fn new(event_type: &str, priority: &str, description: &str, status: &str) -> Self {
-        Event {
-            event_type: event_type.to_string(),
-            priority: match priority {
-                "High" => Priority::High,
-                "Medium" => Priority::Medium,
-                "Low" => Priority::Low,
-                _ => Priority::Low,
-            },
-            description: description.to_string(),
-            status: status.to_string(),
-            event_time: chrono::Utc::now().to_rfc3339(),
-            id: Uuid::new_v4(),
-        }
-    }
-}
-
-impl Transmitter {
-    pub fn send(&self, data: Event) {
-        self.store.lock().unwrap().push_back(data);
-        self.emitter.notify_one();
-    }
-}
-
-impl Receiver {
-    pub fn recv(&self) -> Option<Event> {
-        let mut store = self.store.lock().unwrap();
-
-        while store.is_empty() {
-            store = self.emitter.wait(store).unwrap();
-        }
-
-        store.pop_front()
-    }
-
-    // fn try_recv(&self) -> Option<Event> {
-    //     self.store.lock().unwrap().pop_front()
-    // }
+pub struct Channel {
+    pub tx: Transmitter,
+    pub rx: Receiver,
+    pub producer: Arc<Mutex<KafkaProducer>>,
 }
 
 impl Channel {
     pub fn new() -> Self {
-        let store = Arc::new(Mutex::new(VecDeque::new()));
-        let emitter = Arc::new(Condvar::new());
+        let (sender, receiver) = unbounded();
         let producer = KafkaProducer::new();
 
         Channel {
-            tx: Transmitter {
-                store: Arc::clone(&store),
-                emitter: Arc::clone(&emitter),
-            },
-            rx: Receiver {
-                store: Arc::clone(&store),
-                emitter: Arc::clone(&emitter),
-            },
-            producer,
+            tx: Transmitter { sender },
+            rx: Receiver { receiver },
+            producer: Arc::new(Mutex::new(producer)),
         }
     }
 
-    pub fn start_worker(mut self, _routine_type: RoutineType) -> thread::JoinHandle<()> {
+    pub fn start_worker(
+        &self,
+        worker_id: usize,
+        _routine_type: RoutineType,
+    ) -> thread::JoinHandle<()> {
         let rx = self.rx.clone();
+        let builder = thread::Builder::new();
+        let producer: Arc<Mutex<KafkaProducer>> = Arc::clone(&self.producer);
 
-        thread::spawn(move || {
-            loop {
-                let job = rx.recv(); // we could use try_recv too
+        let handle = builder
+            .spawn(move || {
+                loop {
+                    let job = rx.recv(); // we could use try_recv too
 
-                match job {
-                    Some(mut job) => {
-                        job.status = "picked up by worker".to_owned();
-                        job.event_time = chrono::Utc::now().to_rfc3339();
-                        self.producer.send(job.clone());
+                    match job {
+                        Some(mut job) => {
+                            job.status = "picked up by worker".to_owned();
+                            job.event_time = Utc::now().to_rfc3339();
+                            let mut producer = producer.lock().expect("Failed to lock producer");
+
+                            producer.send(job.clone());
+                            println!("Worker {} picked up job: {:?}", worker_id, job);
+                        }
+                        None => break,
                     }
-                    None => break,
                 }
-            }
-        })
-    }
-}
+            })
+            .expect("Failed to spawn thread");
 
-impl Clone for Receiver {
-    fn clone(&self) -> Self {
-        Receiver {
-            store: Arc::clone(&self.store),
-            emitter: Arc::clone(&self.emitter),
-        }
-    }
-}
-
-impl Clone for Transmitter {
-    fn clone(&self) -> Self {
-        Transmitter {
-            store: Arc::clone(&self.store),
-            emitter: Arc::clone(&self.emitter),
-        }
+        handle
     }
 }
